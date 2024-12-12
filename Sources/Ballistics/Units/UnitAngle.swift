6fe0498ff221211fa979cd4074ec710491c8672d
Raymond Dowe <raydowe@gmail.com>
2024-12-12 18:22:56 +0000

    Fixing conversion for milliradians and minutesOfAngle

--- Sources/Ballistics/Units/UnitAngle.swift
+++ Sources/Ballistics/Units/UnitAngle.swift
@@ -9,5 +9,5 @@ import Foundation
 
 extension UnitAngle {
     public static let minutesOfAngle: UnitAngle = UnitAngle(symbol: "moa", converter: UnitConverterLinear(coefficient: 1/60))
-    public static let milliradians: UnitAngle = UnitAngle(symbol: "mil", converter: UnitConverterLinear(coefficient: 3.43775))
+    public static let milliradians: UnitAngle = UnitAngle(symbol: "mil", converter: UnitConverterLinear(coefficient: 0.0572958))
 }
--- Tests/BallisticsTests/UnitAngleTests.swift
+++ Tests/BallisticsTests/UnitAngleTests.swift
@@ -11,10 +11,8 @@ import Ballistics
 import Numerics
 
 @Test func testUnitAngle() async throws {
-    #expect(Measurement<UnitAngle>(value: 4.7, unit: .milliradians).converted(to: .degrees).value.isApproximatelyEqual(to: 16.1, absoluteTolerance: 0.1))
-    #expect(Measurement<UnitAngle>(value: 2, unit: .degrees).converted(to: .milliradians).value.isApproximatelyEqual(to: 0.58, absoluteTolerance: 0.01))
-
-    #expect(Measurement<UnitAngle>(value: 1, unit: .minutesOfAngle).converted(to: .degrees).value.isApproximatelyEqual(to: 0.0166667, absoluteTolerance: 0.0000001))
-    #expect(Measurement<UnitAngle>(value: 1, unit: .degrees).converted(to: .minutesOfAngle).value.isApproximatelyEqual(to: 60, absoluteTolerance: 0.01))
-    #expect(Measurement<UnitAngle>(value: 1, unit: .minutesOfAngle).converted(to: .arcMinutes).value.isApproximatelyEqual(to: 1, absoluteTolerance: 0.0001))
+    #expect(Measurement<UnitAngle>(value: 1, unit: .milliradians).converted(to: .minutesOfAngle).value.isApproximatelyEqual(to: 3.43775, absoluteTolerance: 1e-5))
+    #expect(Measurement<UnitAngle>(value: 1, unit: .milliradians).converted(to: .degrees).value.isApproximatelyEqual(to: 0.0572958, absoluteTolerance: 1e-5))
+    #expect(Measurement<UnitAngle>(value: 1, unit: .minutesOfAngle).converted(to: .milliradians).value.isApproximatelyEqual(to: 0.290888, absoluteTolerance: 1e-5))
+    #expect(Measurement<UnitAngle>(value: 1, unit: .minutesOfAngle).converted(to: .arcMinutes).value.isApproximatelyEqual(to: 1, absoluteTolerance: 1e-4))
 }
